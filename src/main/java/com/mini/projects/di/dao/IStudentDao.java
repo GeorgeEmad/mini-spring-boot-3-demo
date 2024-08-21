499bfd2c6f67907f42979e6ff85bb8be14edd2dd
george.emad <george.emad9819@gmail.com>
2024-08-21 15:28:53 +0300

    add DAO update and delete

--- src/main/java/com/mini/projects/di/dao/IStudentDao.java
+++ src/main/java/com/mini/projects/di/dao/IStudentDao.java
@@ -8,4 +8,11 @@ public interface IStudentDao {
     void save(Student student);
     Student findStudentById(int id);
     List<Student> findAll();
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
 }
--- src/main/java/com/mini/projects/di/dao/StudentDao.java
+++ src/main/java/com/mini/projects/di/dao/StudentDao.java
@@ -35,4 +35,43 @@ public class StudentDao implements IStudentDao {
         TypedQuery <Student> typedQuery = entityManager.createQuery("FROM Student order by lastName", Student.class);
         return typedQuery.getResultList();
     }
+
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+
+        // retrieve the student
+        Student theStudent = entityManager.find(Student.class, id);
+
+        // delete the student
+        entityManager.remove(theStudent);
+    }
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery(
+                "FROM Student WHERE lastName=:theData", Student.class);
+
+        // set query parameters
+        theQuery.setParameter("theData", theLastName);
+
+        // return query results
+        return theQuery.getResultList();
+    }
+    @Override
+    @Transactional
+    public int deleteAll() {
+
+        int numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+
+        return numRowsDeleted;
+    }
+
 }
